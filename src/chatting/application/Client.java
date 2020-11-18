c1bf84d5483fc1165f7962e8aa2b61cf4675aa71
bamblebam <burhanizrangwala@gmail.com>
2020-11-18 20:44:03 +0530

    added comments

--- src/chatting/application/Client.java
+++ src/chatting/application/Client.java
@@ -13,6 +13,7 @@ import java.util.Calendar;
 import java.text.SimpleDateFormat;
 import javax.swing.*;
 
+//Client class that is responsible for user 2
 public class Client implements ActionListener {
 
     JPanel p1;
@@ -20,7 +21,7 @@ public class Client implements ActionListener {
     JButton b1;
     static JPanel a1;
     static JFrame f1 = new JFrame();
-
+    // Creates the box
     static Box vertical = Box.createVerticalBox();
 
     static Socket s;
@@ -29,7 +30,9 @@ public class Client implements ActionListener {
 
     Boolean typing;
 
+    // Constructor
     Client() {
+        // The top navbar
         f1.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         p1 = new JPanel();
@@ -37,7 +40,7 @@ public class Client implements ActionListener {
         p1.setBackground(new Color(7, 94, 84));
         p1.setBounds(0, 0, 450, 70);
         f1.add(p1);
-
+        // Setting icon for exit arrow and adding eventlistener to exit
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("chatting/application/icons/3.png"));
         Image i2 = i1.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
@@ -51,40 +54,41 @@ public class Client implements ActionListener {
             }
         });
 
+        // Setting profile picture for user 2
         ImageIcon i4 = new ImageIcon(ClassLoader.getSystemResource("chatting/application/icons/pp2.png"));
         Image i5 = i4.getImage().getScaledInstance(60, 60, Image.SCALE_DEFAULT);
         ImageIcon i6 = new ImageIcon(i5);
         JLabel l2 = new JLabel(i6);
         l2.setBounds(40, 5, 60, 60);
         p1.add(l2);
-
+        // Setting icon for video
         ImageIcon i7 = new ImageIcon(ClassLoader.getSystemResource("chatting/application/icons/video.png"));
         Image i8 = i7.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT);
         ImageIcon i9 = new ImageIcon(i8);
         JLabel l5 = new JLabel(i9);
         l5.setBounds(290, 20, 30, 30);
         p1.add(l5);
-
+        // Setting icon for phone
         ImageIcon i11 = new ImageIcon(ClassLoader.getSystemResource("chatting/application/icons/phone.png"));
         Image i12 = i11.getImage().getScaledInstance(35, 30, Image.SCALE_DEFAULT);
         ImageIcon i13 = new ImageIcon(i12);
         JLabel l6 = new JLabel(i13);
         l6.setBounds(350, 20, 35, 30);
         p1.add(l6);
-
+        // Setting icon for the 3 dots on the side
         ImageIcon i14 = new ImageIcon(ClassLoader.getSystemResource("chatting/application/icons/3icon.png"));
         Image i15 = i14.getImage().getScaledInstance(13, 25, Image.SCALE_DEFAULT);
         ImageIcon i16 = new ImageIcon(i15);
         JLabel l7 = new JLabel(i16);
         l7.setBounds(410, 20, 13, 25);
         p1.add(l7);
-
+        // Setting the name for user 2
         JLabel l3 = new JLabel("User 2");
         l3.setFont(new Font("SAN_SERIF", Font.BOLD, 18));
         l3.setForeground(Color.WHITE);
         l3.setBounds(110, 15, 100, 18);
         p1.add(l3);
-
+        // Setting the active now label and setting the condition
         JLabel l4 = new JLabel("Active Now");
         l4.setFont(new Font("SAN_SERIF", Font.PLAIN, 14));
         l4.setForeground(Color.WHITE);
@@ -100,14 +104,14 @@ public class Client implements ActionListener {
         });
 
         t.setInitialDelay(2000);
-
+        // Creates the border around the chat section
         a1 = new JPanel();
         a1.setFont(new Font("SAN_SERIF", Font.PLAIN, 16));
 
         JScrollPane sp = new JScrollPane(a1);
         sp.setBounds(5, 75, 440, 570);
         sp.setBorder(BorderFactory.createEmptyBorder());
-
+        // Scrollbar logic and UI
         ScrollBarUI ui = new BasicScrollBarUI() {
             protected JButton createDecreaseButton(int orientation) {
                 JButton button = super.createDecreaseButton(orientation);
@@ -127,6 +131,7 @@ public class Client implements ActionListener {
 
         sp.getVerticalScrollBar().setUI(ui);
         f1.add(sp);
+        // Additional logic for setting typing or active now
         t1 = new JTextField();
         t1.setBounds(5, 655, 310, 40);
         t1.setFont(new Font("SAN_SERIF", Font.PLAIN, 16));
@@ -149,7 +154,7 @@ public class Client implements ActionListener {
                 }
             }
         });
-
+        // Setting the send button
         b1 = new JButton("Send");
         b1.setBounds(320, 655, 123, 40);
         b1.setBackground(new Color(7, 94, 84));
@@ -167,6 +172,7 @@ public class Client implements ActionListener {
 
     }
 
+    // logic for sending the message
     public void actionPerformed(ActionEvent ae) {
 
         try {
@@ -183,7 +189,6 @@ public class Client implements ActionListener {
 
             a1.add(vertical, BorderLayout.PAGE_START);
 
-            // a1.add(p2);
             dout.writeUTF(out);
             t1.setText("");
         } catch (Exception e) {
@@ -191,6 +196,7 @@ public class Client implements ActionListener {
         }
     }
 
+    // Speech bubble
     public static JPanel formatLabel(String out) {
         JPanel p3 = new JPanel();
         p3.setLayout(new BoxLayout(p3, BoxLayout.Y_AXIS));
@@ -212,6 +218,7 @@ public class Client implements ActionListener {
         return p3;
     }
 
+    // main function for setting server and ip stuff
     public static void main(String[] args) {
         new Client().f1.setVisible(true);
 
--- src/chatting/application/Server.java
+++ src/chatting/application/Server.java
@@ -13,6 +13,7 @@ import java.io.*;
 import java.util.Calendar;
 import java.text.SimpleDateFormat;
 
+//Server class that is responsible for user 1
 public class Server implements ActionListener {
 
     JPanel p1;
@@ -21,6 +22,7 @@ public class Server implements ActionListener {
     static JPanel a1;
     static JFrame f1 = new JFrame();
 
+    // Creates the box
     static Box vertical = Box.createVerticalBox();
 
     static ServerSocket skt;
@@ -30,7 +32,9 @@ public class Server implements ActionListener {
 
     Boolean typing;
 
+    // Constructor
     Server() {
+        // The top navbar
         f1.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         p1 = new JPanel();
         p1.setLayout(null);
@@ -38,6 +42,7 @@ public class Server implements ActionListener {
         p1.setBounds(0, 0, 450, 70);
         f1.add(p1);
 
+        // Setting icon for exit arrow and adding eventlistener to exit
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("chatting/application/icons/3.png"));
         Image i2 = i1.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
@@ -51,6 +56,7 @@ public class Server implements ActionListener {
             }
         });
 
+        // Setting profile picture for user 1
         ImageIcon i4 = new ImageIcon(ClassLoader.getSystemResource("chatting/application/icons/pp1.png"));
         Image i5 = i4.getImage().getScaledInstance(60, 60, Image.SCALE_DEFAULT);
         ImageIcon i6 = new ImageIcon(i5);
@@ -58,6 +64,7 @@ public class Server implements ActionListener {
         l2.setBounds(40, 5, 60, 60);
         p1.add(l2);
 
+        // Setting icon for video
         ImageIcon i7 = new ImageIcon(ClassLoader.getSystemResource("chatting/application/icons/video.png"));
         Image i8 = i7.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT);
         ImageIcon i9 = new ImageIcon(i8);
@@ -65,26 +72,27 @@ public class Server implements ActionListener {
         l5.setBounds(290, 20, 30, 30);
         p1.add(l5);
 
+        // Setting icon for phone
         ImageIcon i11 = new ImageIcon(ClassLoader.getSystemResource("chatting/application/icons/phone.png"));
         Image i12 = i11.getImage().getScaledInstance(35, 30, Image.SCALE_DEFAULT);
         ImageIcon i13 = new ImageIcon(i12);
         JLabel l6 = new JLabel(i13);
         l6.setBounds(350, 20, 35, 30);
         p1.add(l6);
-
+        // Setting icon for the 3 dots on the side
         ImageIcon i14 = new ImageIcon(ClassLoader.getSystemResource("chatting/application/icons/3icon.png"));
         Image i15 = i14.getImage().getScaledInstance(13, 25, Image.SCALE_DEFAULT);
         ImageIcon i16 = new ImageIcon(i15);
         JLabel l7 = new JLabel(i16);
         l7.setBounds(410, 20, 13, 25);
         p1.add(l7);
-
+        // Setting the name for user 1
         JLabel l3 = new JLabel("User 1");
         l3.setFont(new Font("SAN_SERIF", Font.BOLD, 18));
         l3.setForeground(Color.WHITE);
         l3.setBounds(110, 15, 100, 18);
         p1.add(l3);
-
+        // Setting the active now label and setting the condition
         JLabel l4 = new JLabel("Active Now");
         l4.setFont(new Font("SAN_SERIF", Font.PLAIN, 14));
         l4.setForeground(Color.WHITE);
@@ -100,14 +108,14 @@ public class Server implements ActionListener {
         });
 
         t.setInitialDelay(2000);
-
+        // Creates the border around the chat section
         a1 = new JPanel();
         a1.setFont(new Font("SAN_SERIF", Font.PLAIN, 16));
 
         JScrollPane sp = new JScrollPane(a1);
         sp.setBounds(5, 75, 440, 570);
         sp.setBorder(BorderFactory.createEmptyBorder());
-
+        // Scrollbar logic and UI
         ScrollBarUI ui = new BasicScrollBarUI() {
             protected JButton createDecreaseButton(int orientation) {
                 JButton button = super.createDecreaseButton(orientation);
@@ -127,7 +135,7 @@ public class Server implements ActionListener {
 
         sp.getVerticalScrollBar().setUI(ui);
         f1.add(sp);
-
+        // Additional logic for setting typing or active now
         t1 = new JTextField();
         t1.setBounds(5, 655, 310, 40);
         t1.setFont(new Font("SAN_SERIF", Font.PLAIN, 16));
@@ -151,6 +159,7 @@ public class Server implements ActionListener {
             }
         });
 
+        // Setting the send button
         b1 = new JButton("Send");
         b1.setBounds(320, 655, 123, 40);
         b1.setBackground(new Color(7, 94, 84));
@@ -168,6 +177,7 @@ public class Server implements ActionListener {
 
     }
 
+    // logic for sending the message
     public void actionPerformed(ActionEvent ae) {
         try {
             String out = t1.getText();
@@ -183,7 +193,6 @@ public class Server implements ActionListener {
 
             a1.add(vertical, BorderLayout.PAGE_START);
 
-            // a1.add(p2);
             dout.writeUTF(out);
             t1.setText("");
         } catch (Exception e) {
@@ -191,6 +200,7 @@ public class Server implements ActionListener {
         }
     }
 
+    // Speech bubble
     public static JPanel formatLabel(String out) {
         JPanel p3 = new JPanel();
         p3.setLayout(new BoxLayout(p3, BoxLayout.Y_AXIS));
@@ -212,6 +222,7 @@ public class Server implements ActionListener {
         return p3;
     }
 
+    // main function for setting server and ip stuff
     public static void main(String[] args) {
         new Server().f1.setVisible(true);
 
